e0a3fd9d49327b4539a74859fafe328d759c998f
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-07-25 18:25:17 -0700

    Add argument parsing

--- src/main/scala/net/rhysre/qotd/app.scala
+++ src/main/scala/net/rhysre/qotd/app.scala
@@ -2,15 +2,43 @@ package net.rhysre.scalaquote
 
 import java.lang.Thread
 
-object Qotd extends App {
-  val reader = new QuoteReader
+object Qotd {
+  case class Config (port: Option[Int] = None)
 
-  val tcpQuoteServer = new Thread(new TcpQuoteServer(9999, reader))
-  val udpQuoteServer = new Thread(new UdpQuoteServer(9999, reader))
+  def main(args: Array[String]) = {
+    val config = parseArgs(new Config, args.toList)
 
-  tcpQuoteServer.start
-  udpQuoteServer.start
+    val reader = new QuoteReader
+    val port = config.port match {
+      case Some(port) => port
+      case None => 17 // Default port for QOTD servers is 17
+    }
 
-  tcpQuoteServer.join
-  udpQuoteServer.join
+    val tcpThread = startTcpServer(port, reader)
+    val udpThread = startUdpServer(port, reader)
+
+    tcpThread.join()
+    udpThread.join()
+  }
+
+  def parseArgs(config: Config, argList: List[String]): Config = {
+    argList match {
+      case "--port" :: port :: tail => parseArgs(config.copy(port = Some(port.toInt)), tail)
+      case option :: value :: tail => parseArgs(config, tail)
+      case option :: Nil => config
+      case Nil => config
+    }
+  }
+
+  def startTcpServer(port: Int, quoteReader: QuoteReader): Thread = {
+    val thread = new Thread(new TcpQuoteServer(port, quoteReader))
+    thread.start
+    thread
+  }
+
+  def startUdpServer(port: Int, quoteReader: QuoteReader): Thread = {
+    val thread = new Thread(new UdpQuoteServer(port, quoteReader))
+    thread.start
+    thread
+  }
 }
